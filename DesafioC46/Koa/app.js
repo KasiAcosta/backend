ba2e70b60ee4c630e4d5fb1e0837312a594f2795
Kasi Acosta <kasiiacosta@gmail.com>
2023-02-14 22:45:54 -0300

    Desafio C46

---
+++ DesafioC46/Koa/app.js
@@ -0,0 +1,16 @@
+const Koa = require('koa');
+const koaBody = require('koa-body');
+
+const app = new Koa();
+
+app.use(koaBody());
+
+let productos = require('./productos.js');
+
+app.use(productos.routes());
+
+const PORT = 8080
+const server = app.listen(PORT, () =>{
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+})
+server.on('error', error =>console.log('Error en servidor:', error))
